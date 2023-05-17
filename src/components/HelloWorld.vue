e0a945ba54c7d15d4813a0da3506b3d818eed542
vlad_mikhalev <v.a.mikhalev@mail.ru>
2023-05-17 21:38:04 +0400

    make pagination

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -34,7 +34,7 @@
       <div id="content" class="site-content">
         <div id="primary" class="content-area column full">
           <main v-if="posts.length > 0" id="main" class="site-main">
-            <div class="grid portfoliogrid">
+            <div class="grid portfoliogrid articleList">
 
               <!-- :src="post.image"  -->
 
@@ -42,7 +42,7 @@
                 <header class="entry-header">
                   <div class="entry-thumbnail">
                     <a href="portfolio-item.html">
-                      <img src="http://s3.amazonaws.com/caymandemo/wp-content/uploads/sites/15/2015/09/30162427/p1.jpg"
+                      <img src="https://cdn.dribbble.com/userupload/7067483/file/original-951562a758c87c94e5e8e9df607ca127.jpg?compress=1&resize=752x"
                         class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="p1" />
                     </a>
                   </div>
@@ -53,118 +53,16 @@
                 </header>
               </article>
 
-              <!-- 
-                <article class="hentry">
-                <header class="entry-header">
-                  <div class="entry-thumbnail">
-                    <a href="portfolio-item.html">
-                      <img src="http://s3.amazonaws.com/caymandemo/wp-content/uploads/sites/15/2015/09/30162427/p1.jpg"
-                        class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="p1" />
-                    </a>
-                  </div>
-                  <h2 class="entry-title"><a href="portfolio-item.html" rel="bookmark">Sunset Beach</a></h2>
-                  <a class='portfoliotype' href='portfolio-category.html'>summer</a>
-                  <a class='portfoliotype' href='portfolio-category.html'>woman</a>
-                  <a class='portfoliotype' href='portfolio-category.html'>yellow</a>
-                </header>
-              </article>
-
-              <article class="hentry">
-                <header class="entry-header">
-                  <div class="entry-thumbnail">
-                    <a href="portfolio-item.html"><img
-                        src="http://s3.amazonaws.com/caymandemo/wp-content/uploads/sites/15/2015/09/30160348/p5.jpg"
-                        class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="p1" /></a>
-                  </div>
-                  <h2 class="entry-title"><a href="portfolio-item.html" rel="bookmark">Earl of Moreland</a></h2>
-                  <a class='portfoliotype' href='portfolio-category.html'>hat</a>
-                  <a class='portfoliotype' href='portfolio-category.html'>woman</a>
-                  <a class='portfoliotype' href='portfolio-category.html'>yellow</a>
-                </header>
-              </article>
-
-              <article class="hentry">
-                <header class="entry-header">
-                  <div class="entry-thumbnail">
-                    <a href="portfolio-item.html"><img
-                        src="http://s3.amazonaws.com/caymandemo/wp-content/uploads/sites/15/2015/09/30160151/p3.jpg"
-                        class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="p1" /></a>
-                  </div>
-                  <h2 class="entry-title"><a href="portfolio-item.html" rel="bookmark">Eliza and John</a></h2>
-                  <a class='portfoliotype' href='portfolio-category.html'>summer</a>
-                  <a class='portfoliotype' href='portfolio-category.html'>woman</a>
-                  <a class='portfoliotype' href='portfolio-category.html'>yellow</a>
-                </header>
-              </article>
-
-              <article class="hentry">
-                <header class="entry-header">
-                  <div class="entry-thumbnail">
-                    <a href="portfolio-item.html"><img
-                        src="http://s3.amazonaws.com/caymandemo/wp-content/uploads/sites/15/2015/09/18160911/p6.jpg"
-                        class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="p1" /></a>
-                  </div>
-                  <h2 class="entry-title"><a href="portfolio-item.html" rel="bookmark">Hot Afternoon</a></h2>
-                  <a class='portfoliotype' href='portfolio-category.html'>pink</a>
-                  <a class='portfoliotype' href='portfolio-category.html'>woman</a>
-                  <a class='portfoliotype' href='portfolio-category.html'>yellow</a>
-                </header>
-              </article>
-
-              <article class="hentry">
-                <header class="entry-header">
-                  <div class="entry-thumbnail">
-                    <a href="portfolio-item.html"><img
-                        src="http://s3.amazonaws.com/caymandemo/wp-content/uploads/sites/15/2015/09/15223245/p2.jpg"
-                        class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="p1" /></a>
-                  </div>
-                  <h2 class="entry-title"><a href="portfolio-item.html" rel="bookmark">Long Walks</a></h2>
-                  <a class='portfoliotype' href='portfolio-category.html'>hat</a>
-                  <a class='portfoliotype' href='portfolio-category.html'>summer</a>
-                  <a class='portfoliotype' href='portfolio-category.html'>yellow</a>
-                </header>
-              </article>
-
-              <article class="hentry">
-                <header class="entry-header">
-                  <div class="entry-thumbnail">
-                    <a href="portfolio-item.html"><img
-                        src="http://s3.amazonaws.com/caymandemo/wp-content/uploads/sites/15/2015/09/15222855/p7.jpg"
-                        class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="p1" /></a>
-                  </div>
-                  <h2 class="entry-title"><a href="portfolio-item.html" rel="bookmark">Twilight</a></h2>
-                  <a class='portfoliotype' href='portfolio-category.html'>hat</a>
-                  <a class='portfoliotype' href='portfolio-category.html'>woman</a>
-                  <a class='portfoliotype' href='portfolio-category.html'>summer</a>
-                </header>
-              </article> -->
-
             </div>
             <!-- .grid -->
 
-
-
-
-
-            <vue-awesome-paginate :total-items="totalPages" :items-per-page="2" :max-pages-shown="limit" v-model="page"
-              :on-click="changePage" />
-            <!-- <nav class="pagination">
-              <span class="page-numbers current">1</span>
-              <a class="page-numbers" href="#">2</a>
-              <a class="next page-numbers" href="#">Next »</a>
-            </nav> -->
-            <nav class="pagination">
-              <div class="page-numbers" v-for="pageNumber in totalPages" :key="pageNumber"
-                :class="{ 'current': page === pageNumber }" @click="changePage(pageNumber)">
-                {{ pageNumber }}
-              </div>
-
-            </nav>
+            <vue-awesome-paginate :total-items="50" v-model="page" :items-per-page="5" :max-pages-shown="5"
+              :on-click="changePage" :hide-prev-next-when-ends="true" />
 
             <br />
 
           </main>
-          <h2 v-else>Нет статей</h2>
+          <h2 style="text-align: center;" v-else>Нет статей</h2>
           <!-- #main -->
         </div>
         <!-- #primary -->
@@ -177,7 +75,6 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import axios from 'axios';
-import { LOGICAL_OPERATORS } from '@babel/types';
 
 interface IPost {
   id: string;
@@ -193,19 +90,20 @@ export default defineComponent({
   methods: {
     changePage(pageNumber: number) {
       this.page = pageNumber;
-      this.fetchPosts();
+      
     },
     async fetchPosts() {
       try {
-        const response = await axios.get('https://6082e3545dbd2c001757abf5.mockapi.io/qtim-test-work/posts/');
+        const response = await axios.get('https://6082e3545dbd2c001757abf5.mockapi.io/qtim-test-work/posts', {
+          params: {
+            page: this.page,
+            limit: this.limit,
+          },
+        });
         const data = response.data;
         console.log(data);
-        const currentPosts = response.data.slice(this.indexPages, this.indexPages + 9);
-        this.indexPages = this.indexPages + this.limit;
-        console.log(currentPosts);
         this.totalPages = Math.ceil(data.length / this.limit);
-        this.posts = currentPosts;
-        console.log(this.posts);
+        this.posts = data;
       } catch (error) {
         alert(error);
       }
@@ -215,45 +113,57 @@ export default defineComponent({
     return {
       page: 1,
       limit: 9,
-      indexPages: 0,
       totalPages: 0,
       isPostsLoading: false,
       posts: [] as IPost[],
-      // posts: [
-        // {
-        //   id: "1",
-        //   createdAt: "2021-04-23T05:11:54.250Z",
-        //   title: "rrrrrrrrrrrrrrr",
-        //   preview: "programming the port won't do anything, we need to copy the auxiliary USB system!",
-        //   image: "http://lorempixel.com/640/480/technics",
-        //   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        // },
-        // {
-        //   id: "2",
-        //   createdAt: "2021-04-23T05:11:54.250Z",
-        //   title: "qqqqqqqqqqq",
-        //   preview: "programming the port won't do anything, we need to copy the auxiliary USB system!",
-        //   image: "http://lorempixel.com/640/480/technics",
-        //   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        // },
-        // {
-        //   id: "3",
-        //   createdAt: "2021-04-23T05:11:54.250Z",
-        //   title: "wwwwwwwwwwww",
-        //   preview: "programming the port won't do anything, we need to copy the auxiliary USB system!",
-        //   image: "http://lorempixel.com/640/480/technics",
-        //   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        // },
-      // ],
 
     }
   },
   created() {
     this.fetchPosts();
   },
+  watch: {
+    page() {
+      this.fetchPosts();
+    }
+  }
 });
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.pagination-container {
+  display: flex;
+  column-gap: 10px;
+}
+
+.paginate-buttons {
+  height: 40px;
+  width: 40px;
+  border-radius: 20px;
+  cursor: pointer;
+  background-color: rgb(242, 242, 242);
+  border: 1px solid rgb(217, 217, 217);
+  color: black;
+}
+
+.paginate-buttons:hover {
+  background-color: #d8d8d8;
+}
+
+.active-page {
+  background-color: #3498db;
+  border: 1px solid #3498db;
+  color: white;
+}
+
+.active-page:hover {
+  background-color: #2988c8;
+}
+
+.articleList {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
 </style>
